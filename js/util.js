f5e322ec6c069c12bf43c2ef81b5449fea6f99ee
Довгомеля Анастасия <dovgomelaanastasia@gmail.com>
2025-12-08 19:34:18 +0500

    module 5 task 1

--- js/util.js
+++ js/util.js
@@ -2,16 +2,31 @@ function getRandomInt(min, max) {
     if (min < 0 || max < 0) {
       return -1;
     }
-    
+  
     if (min > max) {
       [min, max] = [max, min];
     }
-    
+  
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
   function getRandomArrayElement(elements) {
     return elements[getRandomInt(0, elements.length - 1)];
+  }
+  
+  function createRandomIdFromRangeGenerator(min, max) {
+    const previousValues = [];
+  
+    return function() {
+      let currentValue = getRandomInt(min, max);
+  
+      while (previousValues.includes(currentValue)) {
+        currentValue = getRandomInt(min, max);
+      }
+  
+      previousValues.push(currentValue);
+      return currentValue;
+    };
   }
   
   function checkStringLength(string, maxLength) {
@@ -27,12 +42,12 @@ function getRandomInt(min, max) {
   function extractNumber(value) {
     const string = String(value);
     const digits = string.match(/\d/g);
-    
+  
     if (!digits) {
       return NaN;
     }
-    
+  
     return parseInt(digits.join(''), 10);
   }
   
-  export { getRandomInt, getRandomArrayElement, checkStringLength, isPalindrome, extractNumber };
+  export { getRandomInt, getRandomArrayElement, createRandomIdFromRangeGenerator, checkStringLength, isPalindrome, extractNumber };
