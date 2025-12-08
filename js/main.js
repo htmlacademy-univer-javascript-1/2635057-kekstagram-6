837868f565ee7fa5024b3ef8a7aab88385c46fad
Довгомеля Анастасия <dovgomelaanastasia@gmail.com>
2025-12-08 21:18:08 +0500

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { createPhotos } from './data.js';
 import { renderPictures } from './picture-render.js';
+import './form-handler.js';
 
 const photos = createPhotos();
 renderPictures(photos);
